a095e8a01d9b0b83f64800d7a0967e65ce6f9ea4
Lovro Šubelj <lovro.subelj@fri.uni-lj.si>
2023-04-16 18:18:52 +0200

    Update INA labs

--- ina/code/ix.py
+++ ina/code/ix.py
@@ -127,15 +127,15 @@ def comparison(G, algs, runs = 1):
   
 algs = {"Known": lambda G: clustering(G), "Infomap": algorithms.infomap, "Louvain": algorithms.louvain, "Leiden": algorithms.leiden, "LPA": algorithms.label_propagation, "FLPA": fast_label_propagation, "MCL": algorithms.markov_clustering, "EB": lambda G: algorithms.girvan_newman(G, level = 1)}
 
-#for name in ["karate_club", "dolphins", "american_football"]:
-#  G = read(name)
-#
-#  info(G)
-#
-#  comparison(G, algs, runs = 100)
-#
-#  #viz.plot_network_clusters(G, algs["Known"](G), nx.spring_layout(G))
-#  #plt.show()
+for name in ["karate_club", "dolphins", "american_football"]:
+  G = read(name)
+
+  info(G)
+
+  comparison(G, algs, runs = 100)
+
+  #viz.plot_network_clusters(G, algs["Known"](G), nx.spring_layout(G))
+  #plt.show()
 
 ###
 ### Community detection in networks with node metadata
@@ -144,59 +144,59 @@ algs = {"Known": lambda G: clustering(G), "Infomap": algorithms.infomap, "Louvai
 algs.pop("MCL")
 algs.pop("EB")
 
-#for name in ["cdn_java", "wikileaks", "dormitory"]:
-#  G = read(name)
-#
-#  info(G)
-#
-#  comparison(G, algs, runs = 10)
+for name in ["cdn_java", "wikileaks", "dormitory"]:
+  G = read(name)
+
+  info(G)
+
+  comparison(G, algs, runs = 10)
 
 ###
 ### Community detection in Girvan-Newman benchmark graphs
 ###
 
-#def girvan_newman(mu = 0.1):
-#  G = nx.MultiGraph(name = "girvan_newman")
-#  for i in range(128):
-#    G.add_node(i, cluster = i // 32 + 1)
-#
-#  for i in range(128):
-#    for j in range(i + 1, 128):
-#      if G.nodes[i]['cluster'] == G.nodes[j]['cluster']:
-#        if random.random() < 16 * (1 - mu) / 31:
-#          G.add_edge(i, j)
-#      else:
-#        if random.random() < mu / 6:
-#          G.add_edge(i, j)
-#
-#  return G
-#
-#info(girvan_newman())
-#
-#runs = 100
-#mus = np.arange(0, 1, 0.05)
-#
-#plt.figure()
-#
-#for alg in algs:
-#  NMI = [0 for _ in range(len(mus))]
-#
-#  for _ in range(runs):
-#    for i, mu in enumerate(mus):
-#      G = girvan_newman(mu)
-#
-#      K = clustering(G)
-#      C = algs[alg](G)
-#
-#      NMI[i] += K.normalized_mutual_information(C).score / runs
-#
-#  plt.plot(mus, NMI, label = alg)
-#
-#plt.xlabel("Mixing $\mu$")
-#plt.ylabel("NMI")
-#plt.legend()
-#
-#plt.savefig("girvan_newman.pdf", bbox_inches = 'tight')
+def girvan_newman(mu = 0.1):
+  G = nx.MultiGraph(name = "girvan_newman")
+  for i in range(128):
+    G.add_node(i, cluster = i // 32 + 1)
+
+  for i in range(128):
+    for j in range(i + 1, 128):
+      if G.nodes[i]['cluster'] == G.nodes[j]['cluster']:
+        if random.random() < 16 * (1 - mu) / 31:
+          G.add_edge(i, j)
+      else:
+        if random.random() < mu / 6:
+          G.add_edge(i, j)
+
+  return G
+
+info(girvan_newman())
+
+runs = 100
+mus = np.arange(0, 1, 0.05)
+
+plt.figure()
+
+for alg in algs:
+  NMI = [0 for _ in range(len(mus))]
+
+  for _ in range(runs):
+    for i, mu in enumerate(mus):
+      G = girvan_newman(mu)
+
+      K = clustering(G)
+      C = algs[alg](G)
+
+      NMI[i] += K.normalized_mutual_information(C).score / runs
+
+  plt.plot(mus, NMI, label = alg)
+
+plt.xlabel("Mixing $\mu$")
+plt.ylabel("NMI")
+plt.legend()
+
+plt.savefig("girvan_newman.pdf", bbox_inches = 'tight')
 
 ###
 ### Community detection in Erdos-Renyi random graphs
@@ -204,42 +204,42 @@ algs.pop("EB")
 
 algs.pop("Leiden")
 
-#def erdos_renyi(n = 1000, k = 10):
-#  G = nx.MultiGraph(nx.gnm_random_graph(n, n * k // 2))
-#  G.name = "erdos_renyi"
-#
-#  for c, C  in enumerate(nx.connected_components(G)):
-#    for i in C:
-#      G.nodes[i]['cluster'] = c
-#
-#  return G
-#
-#info(erdos_renyi())
-#
-#runs = 10
-#ks = range(24)
-#
-#plt.figure()
-#
-#for alg in algs:
-#  NVI = [0 for _ in range(len(ks))]
-#
-#  for _ in range(runs):
-#    for i, k in enumerate(ks):
-#      G = erdos_renyi(k = k)
-#
-#      K = clustering(G)
-#      C = algs[alg](G)
-#
-#      NVI[i] += K.variation_of_information(C).score / math.log(len(G)) / runs
-#
-#  plt.plot(ks, NVI, label = alg)
-#
-#plt.xlabel("Degree $k$")
-#plt.ylabel("NVI")
-#plt.legend()
-#
-#plt.savefig("erdos_renyi.pdf", bbox_inches = 'tight')
+def erdos_renyi(n = 1000, k = 10):
+  G = nx.MultiGraph(nx.gnm_random_graph(n, n * k // 2))
+  G.name = "erdos_renyi"
+
+  for c, C  in enumerate(nx.connected_components(G)):
+    for i in C:
+      G.nodes[i]['cluster'] = c
+
+  return G
+
+info(erdos_renyi())
+
+runs = 10
+ks = range(24)
+
+plt.figure()
+
+for alg in algs:
+  NVI = [0 for _ in range(len(ks))]
+
+  for _ in range(runs):
+    for i, k in enumerate(ks):
+      G = erdos_renyi(k = k)
+
+      K = clustering(G)
+      C = algs[alg](G)
+
+      NVI[i] += K.variation_of_information(C).score / math.log(len(G)) / runs
+
+  plt.plot(ks, NVI, label = alg)
+
+plt.xlabel("Degree $k$")
+plt.ylabel("NVI")
+plt.legend()
+
+plt.savefig("erdos_renyi.pdf", bbox_inches = 'tight')
 
 ###
 ### Blockmodeling of small social networks
@@ -250,12 +250,12 @@ algs.pop("Leiden")
 algs.pop("Infomap")
 algs["DCSBM"] = algorithms.sbm_dl
 
-#for name in ["karate_club", "southern_women"]:
-#  G = read(name)
-#
-#  info(G)
-#
-#  comparison(G, algs, runs = 100)
+for name in ["karate_club", "southern_women"]:
+  G = read(name)
+
+  info(G)
+
+  comparison(G, algs, runs = 100)
 
 ###
 ### Blockmodeling of Šubelj-Bajec benchmark graphs
