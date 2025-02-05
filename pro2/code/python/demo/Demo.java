eee9fbdec6c31b2b5ab6ad49e2daf6c585657e4d
Lovro Šubelj <lovro.subelj@fri.uni-lj.si>
2025-02-05 15:16:46 +0100

    Add PRO2 lectures

---
+++ pro2/code/python/demo/Demo.java
@@ -0,0 +1,49 @@
+public class Demo {
+
+  public static void main(String[] args) {
+    System.out.println("Pozdravljeni pri predmetu PRO2!");
+    
+    int x = 1;
+    double y;
+    y = 1.23;
+    boolean z = true;
+    char ch = 'a';
+    String st = "niz znakov";
+
+    final double G = 9.81;
+
+    if (x < 1) {
+      System.out.println("Vrednost spremenljivke x je manjša od 1");
+    }
+    else if (x < 2)
+      System.out.println("Vrednost spremenljivke x je med 1 in 2");
+    else {
+      System.out.println("Vrednost spremenljivke x je večja ali enaka 2");
+    }
+     
+    for (int i = 0; i < 5; i++)
+      System.out.println("Vrednost spremenljivke i je enaka " + i);
+     
+    int i = 0;
+    while (i < 5) {
+      System.out.println("Vrednost spremenljivke i je enaka " + i);
+      i++;
+    }
+    
+    method(7);
+    
+    function(2);
+  }
+      
+  public static void method(int i) {
+    System.out.println("Vrednost parametra i je enaka " + i);
+  }
+
+  public static int function(int i) {
+    System.out.println("Vrednost parametra i je enaka " + i);
+    i *= 7;
+    System.out.println("Vrednost rezultata funkcije je enaka " + i);
+    return i;
+  }
+  
+}
---
+++ pro2/code/python/demo/demo.py
@@ -0,0 +1,45 @@
+print('Pozdravljeni pri predmetu PRO2!')
+
+x = 1
+y = 1.23
+z = True
+ch = 'a'
+st = "niz znakov"
+
+G = 9.81
+
+if x < 1:
+	print("Vrednost spremenljivke x je manjša od 1")
+elif x < 2:
+	print("Vrednost spremenljivke x je med 1 in 2")
+else:
+	print("Vrednost spremenljivke x je večja ali enaka 2")
+ 
+for i in range(5):
+	print("Vrednost spremenljivke i je enaka " + str(i))
+ 
+i = 0
+while i < 5:
+	print("Vrednost spremenljivke i je enaka " + str(i))
+	i += 1
+ 
+def method(i):
+	print("Vrednost parametra i je enaka " + str(i))
+  
+method(7)
+
+def function(i):
+	print("Vrednost parametra i je enaka " + str(i))
+	i *= 7
+	print("Vrednost rezultata funkcije je enaka " + str(i))
+	return i
+
+function(2)
+
+inc = lambda x: x + 1
+
+print(inc(0))
+
+pow = lambda x, p: x**p
+
+print(pow(2, 10))
