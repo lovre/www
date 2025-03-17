729df2e882da7d316cccaaef6541ff5f392d44e8
Lovro Šubelj <lovro.subelj@fri.uni-lj.si>
2025-03-17 14:47:38 +0100

    Add INA labs

---
+++ ina/labs/vi-starter.py
@@ -0,0 +1,44 @@
+import networkx as nx
+
+def read(name, path = "."):
+  G = nx.Graph(name = name)
+  with open(path + "/" + name + ".net", 'r') as file:
+    file.readline()
+
+    for line in file:
+      if line.startswith("*"):
+        break
+      else:
+        node = line.split("\"")
+        G.add_node(int(node[0]) - 1, label = node[1])
+
+    for line in file:
+      i, j = (int(x) - 1 for x in line.split()[:2])
+      G.add_edge(i, j)
+      
+  return G
+  
+def info(G):
+  print("{:>12s} | '{:s}'".format('Graph', G.name))
+
+  n = G.number_of_nodes()
+  m = G.number_of_edges()
+  
+  print("{:>12s} | {:,d} ({:,d})".format('Nodes', n, nx.number_of_isolates(G)))
+  print("{:>12s} | {:,d} ({:,d})".format('Edges', m, nx.number_of_selfloops(G)))
+  print("{:>12s} | {:.2f} ({:,d})".format('Degree', 2 * m / n, max([k for _, k in G.degree()])))
+  
+  C = list(nx.connected_components(G))
+
+  print("{:>12s} | {:.1f}% ({:,d})".format('LCC', 100 * max(len(c) for c in C) / n, len(C)))
+  print()
+  
+def tops(G, C, centrality, n = 15):
+  print("{:>12s} | '{:s}'".format('Centrality', centrality))
+  for i, c in sorted(C.items(), key = lambda item: (item[1], G.degree[item[0]]), reverse = True)[:n]:
+    print("{:>12.6f} | '{:s}' ({:,d})".format(c, G.nodes[i]['label'], G.degree[i]))
+  print()
+  
+G = read("collaboration_imdb")
+  
+info(G)
