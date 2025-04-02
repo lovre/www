3caf68e827c67b91e92672c59b029c4667e60967
Lovro Šubelj <lovro.subelj@fri.uni-lj.si>
2025-04-02 19:33:01 +0200

    Update PRO2 lectures

--- pro2/code/python/edges/edges.py
+++ pro2/code/python/edges/edges.py
@@ -17,6 +17,12 @@ for i in range(1, h - 1):
     pixels = img[i - 1:i + 2, j - 1:j + 2, 0]
     hor = abs((MASK * pixels).sum()) / 4
     ver = abs((np.transpose(MASK) * pixels).sum()) / 4
-    edg[i, j] = [1 - (hor + ver) / 2] * 3
+    edg[i][j] = 1 - (hor + ver) / 2
+
+thres = np.sort(edg.flatten())[edg.size // 8]
+
+for i in range(h):
+  for j in range(w):
+    edg[i][j] = 0 if edg[i][j][0] < thres else 1
 
 im.imsave('edges.png', edg)
