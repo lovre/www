8d31b481ea2464d36feacc16ec853264f4720a3e
Lovro Šubelj <lovro.subelj@fri.uni-lj.si>
2025-04-24 10:34:40 +0200

    Update PRO2 lectures

--- pro2/code/java/Mathematics/Mathematics.java
+++ pro2/code/java/Mathematics/Mathematics.java
@@ -12,8 +12,8 @@ import java.util.TreeMap;
 public class Mathematics {
 
 	public static void main(String[] args) throws IOException {
-		Map<Mathematic, Double> papers = new HashMap<Mathematic, Double>();
-		/* Map<Mathematic, Double> papers = new TreeMap<Mathematic, Double>(); */ 
+		Map<Mathematic, Double> scores = new HashMap<Mathematic, Double>();
+		/* Map<Mathematic, Double> scores = new TreeMap<Mathematic, Double>(); */ 
 
 		BufferedReader reader = new BufferedReader(new FileReader("mathematics.csv"));
 		
@@ -22,31 +22,31 @@ public class Mathematics {
 			String[] array = line.split(",");
 
 			Mathematic mathematic = new Mathematic(Integer.parseInt(array[0]), array[1] + " " + array[2]);
-			double value = Double.parseDouble(array[6]);
+			double score = Double.parseDouble(array[6]);
 
-			if (papers.containsKey(mathematic))
-				papers.put(mathematic, papers.get(mathematic) + value);
+			if (scores.containsKey(mathematic))
+				scores.put(mathematic, scores.get(mathematic) + score);
 			else
-				papers.put(mathematic, value);
+				scores.put(mathematic, score);
 		}
 
 		reader.close();
 
-		List<Mathematic> list = new ArrayList<Mathematic>(papers.keySet());
+		List<Mathematic> list = new ArrayList<Mathematic>(scores.keySet());
 		Collections.sort(list, new Comparator<Mathematic>() {
 
 			@Override
 			public int compare(Mathematic fst, Mathematic snd) {
-				if (papers.get(fst) == papers.get(snd))
+				if (scores.get(fst) == scores.get(snd))
 					return fst.compareTo(snd);
 
-				return -new Double(papers.get(fst)).compareTo(papers.get(snd));
+				return -scores.get(fst).compareTo(scores.get(snd));
 			}
 
 		});
 
 		for (int i = 0; i < list.size(); i++)
-			System.out.format("%8.1f | %s\n", papers.get(list.get(i)), list.get(i));
+			System.out.format("%8.1f | %s\n", scores.get(list.get(i)), list.get(i));
 	}
 
 }
